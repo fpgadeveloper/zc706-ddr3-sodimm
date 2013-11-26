8bed58e5aa9f1a6dc4883e297c1209df0153af23
Jeff Johnson <jeff@fpgadeveloper.com>
2013-11-26 11:27:54 -0500

    made the memory test more verbose

--- SDK/mem_test/src/mem_test.c
+++ SDK/mem_test/src/mem_test.c
@@ -17,13 +17,13 @@
 
 int main()
 {
-	unsigned int *sodimm;
+	volatile unsigned int *sodimm;
 	unsigned int i;
 	unsigned int value;
 	unsigned int failed;
 
-	//sodimm = (unsigned int *)XPAR_AXI_7SERIES_DDRX_0_S_AXI_BASEADDR;
-	sodimm = (unsigned int *)0x80000000;
+	sodimm = (unsigned int *)XPAR_AXI_7SERIES_DDRX_0_S_AXI_BASEADDR;
+	//sodimm = (unsigned int *)0x80000000;
 
     xil_printf("-------------------------\n\r");
     xil_printf("ZC706 DDR3 SODIMM example\n\r");
@@ -35,16 +35,22 @@ int main()
     xil_printf("Read after writing DEADBEEF : 0x%X\n\r",sodimm[0]);
 
     // Write incrementing values to the SODIMM
+    xil_printf("Writing sequence to memory...");
     for(i = 0;i < SODIMM_WORDS;i++){
     	sodimm[i] = i;
     }
+    xil_printf("Done.\n\r");
 
     // Read and check the written values
+    xil_printf("Verifying memory...\n\r");
     failed = 0;
     for(i = 0;i < SODIMM_WORDS;i++){
     	value = sodimm[i];
+    	if((i & 0xFFFFFF) == 0xFFFFFF){
+    		xil_printf("SODIMM passed up to address 0x%X\n\r",((unsigned int)sodimm)+i*4);
+    	}
     	if(value != i){
-    		xil_printf("SODIMM failed at address 0x%X\n\r",sodimm+i);
+    		xil_printf("SODIMM failed at address 0x%X\n\r",((unsigned int)sodimm)+i*4);
     		failed = 1;
     		break;
     	}
